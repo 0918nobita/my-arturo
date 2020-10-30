e2abbc462622e0262d3af239592f8d5664033efa
0918nobita <nobita.0918@gmail.com>
2020-10-30 12:48:02 +0900

    (fsharp) implement stack

--- fsharp/src/MyArturo/Program.fs
+++ fsharp/src/MyArturo/Program.fs
@@ -1,4 +1,22 @@
-﻿[<EntryPoint>]
+﻿module Program
+
+[<RequireQualifiedAccess>]
+type Stack<'T> =
+    | Empty
+    | Stack of 'T * Stack<'T>
+
+    member inline s.Push x = Stack(x, s)
+    
+    member inline s.Pop () =
+        match s with
+        | Stack(t, _) -> t
+        | Empty -> failwith "Stack underflow"
+
+[<EntryPoint>]
 let main argv =
     printfn "Arturo on .NET Core"
+    let stack = (Stack.Empty).Push 2
+    stack.Pop () |> ignore
+    let stack = stack.Push 3
+    printfn "%d" (stack.Pop ())
     0
